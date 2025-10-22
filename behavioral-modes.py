bb4c7c3ed150ac4d3bd0e3ed47e83430219cc8c4
Robert Twomey <robert@roberttwomey.com>
2025-10-22 16:38:28 -0700

    added lidar-test

--- behavioral-modes.py
+++ behavioral-modes.py
@@ -155,36 +155,8 @@ async def main():
             data = json.loads(response['data']['data'])
             current_motion_switcher_mode = data['name']
             print(f"Current motion mode: {current_motion_switcher_mode}")
-
-
-        # Switch to "normal" mode if not already
-        if current_motion_switcher_mode != "normal":
-            print(f"Switching motion mode from {current_motion_switcher_mode} to 'normal'...")
-            response = await conn.datachannel.pub_sub.publish_request_new(
-                RTC_TOPIC["MOTION_SWITCHER"], 
-                {
-                    "api_id": 1002,
-                    "parameter": {"name": "normal"}
-                }
-            )
-            await asyncio.sleep(1)  # Wait while it stands up
-
-            if response['data']['header']['status']['code'] == 0:
-                data = json.loads(response['data']['data'])
-                current_motion_switcher_mode = data['name']
-                print(f"New motion mode: {current_motion_switcher_mode}")
-
-        # Switch to AI mode
-        print("Switching motion mode to 'AI'...")
-        await conn.datachannel.pub_sub.publish_request_new(
-            RTC_TOPIC["MOTION_SWITCHER"], 
-            {
-                "api_id": 1002,
-                "parameter": {"name": "ai"}
-            }
-        )
+        
         await asyncio.sleep(3)
-
 
         #=== StandUp - WORKING ===
         print("Sending StandUp...")
@@ -194,6 +166,8 @@ async def main():
                 "api_id": SPORT_CMD["StandUp"],
             }
         )
+
+        await asyncio.sleep(3)
         
         # === BalanceStand - WORKING ===
         print("Sending BalanceStand...")
@@ -230,19 +204,20 @@ async def main():
         #     print(f"Current motion mode: {current_motion_switcher_mode}")
 
 
-        # # Perform a "Move Forward" movement
-        # print("Moving forward...")
-        # await conn.datachannel.pub_sub.publish_request_new(
-        #     RTC_TOPIC["SPORT_MOD"], 
-        #     {
-        #         "api_id": SPORT_CMD["Move"],
-        #         "parameter": {"x": 0.5, "y": 0, "z": 0}
-        #     }
-        # )
-
-        # await asyncio.sleep(3)
-
         # Perform a "Move Forward" movement
+        print("Moving forward...")
+        await conn.datachannel.pub_sub.publish_request_new(
+            RTC_TOPIC["SPORT_MOD"], 
+            {
+                "api_id": SPORT_CMD["Move"],
+                "parameter": {"x": 0.5, "y": 0, "z": 0}
+            }
+        )
+
+        await asyncio.sleep(3)
+
+        # === Perform a "Move Forward" movement ===
+
         # print("Moving forward...")
         # await conn.datachannel.pub_sub.publish_request_new(
         #     RTC_TOPIC["SPORT_MOD"], 
@@ -254,17 +229,18 @@ async def main():
 
         # await asyncio.sleep(3)
 
-        # Perform a "Move Backward" movement
-        # print("Moving backward...")
-        # await conn.datachannel.pub_sub.publish_request_new(
-        #     RTC_TOPIC["SPORT_MOD"], 
-        #     {
-        #         "api_id": SPORT_CMD["Move"],
-        #         "parameter": {"x": -0.5, "y": 0, "z": 0}
-        #     }
-        # )
+        # === Perform a "Move Backward" movement ===
 
-        # await asyncio.sleep(3)
+        print("Moving backward...")
+        await conn.datachannel.pub_sub.publish_request_new(
+            RTC_TOPIC["SPORT_MOD"], 
+            {
+                "api_id": SPORT_CMD["Move"],
+                "parameter": {"x": -0.5, "y": 0, "z": 0}
+            }
+        )
+
+        await asyncio.sleep(3)
 
         # print("Starting Leashmode (LeadFollow)...")
         # await conn.datachannel.pub_sub.publish_request_new(
